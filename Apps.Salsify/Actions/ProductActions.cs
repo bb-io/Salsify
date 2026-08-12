f3261f6319f18f4127ff5732ac40089afa91c9c4
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-08-12 10:24:23 +0300

    migrate product download from export polling to 2 endpoints: get product & get properties; rewrite ProductHtmlConverter

--- Apps.Salsify/Actions/ProductActions.cs
+++ Apps.Salsify/Actions/ProductActions.cs
@@ -1,11 +1,9 @@
 using System.Net.Mime;
 using Apps.Salsify.Api;
-using Apps.Salsify.Api.Utility;
 using Apps.Salsify.Constants;
 using Apps.Salsify.Converters.Product;
-using Apps.Salsify.Converters.Product.Models;
 using Apps.Salsify.Extensions;
-using Apps.Salsify.Models.Entities.Export;
+using Apps.Salsify.Helpers;
 using Apps.Salsify.Models.Entities.Product;
 using Apps.Salsify.Models.Identifiers;
 using Apps.Salsify.Models.Requests.Product;
@@ -14,14 +12,11 @@ using Apps.Salsify.Models.Responses.Product;
 using Apps.Salsify.Models.Responses.Product.Api;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Actions;
-using Blackbird.Applications.Sdk.Common.Exceptions;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Blackbird.Applications.SDK.Extensions.FileManagement.Interfaces;
-using Blackbird.Applications.Sdk.Utils.Extensions.Http;
 using Blackbird.Applications.Sdk.Utils.Extensions.Sdk;
 using Blackbird.Filters.Coders;
 using Blackbird.Filters.Shared;
-using RestSharp;
 
 namespace Apps.Salsify.Actions;
 
@@ -80,55 +75,27 @@ public class ProductActions(InvocationContext context, IFileManagementClient fil
         [ActionParameter] ProductIdentifier productIdentifier,
         [ActionParameter] DownloadProductRequest downloadInput)
     {
-        // The filter param in the request below does not work with 'salsify:id' - it returns 'Invalid search query: Unknown error'
-        // And the value that comes from productIdentifier uses product's 'salsify:id', not system ID
-        // So we have to resolve it manually
         var current = await Client.GetCurrentOrgInfo();
-        
         string locale = current.ResolveLocale(downloadInput.Locale);
-        string? nameProperty = current.GetRolePropertyId(RolePropertyNames.ProductName);
-        string productIdProperty = current.GetRolePropertyId(RolePropertyNames.ProductId) ?? 
-                                   throw new PluginMisconfigurationException("This organization has no product ID property configured");
+
+        var getProductRequest = new SalsifyRequest($"products/{productIdentifier.ProductId}");
+        var product = await Client.ExecuteWithErrorHandling<ProductEntity>(getProductRequest);
         
-        var startExportRequest = new SalsifyRequest("export_runs", Method.Post, ApiVersion.Unversioned)
-            .WithJsonBody(new
-            {
-                configuration = new
-                {
-                    format = "json",
-                    include_all_content_locales = true,
-                    include_all_columns = true,
-                    entity_type = "product",
-                    filter = $"='{productIdProperty}':'{productIdentifier.ProductId}'"
-                }
-            });
-        var startExportResponse = await Client.ExecuteWithErrorHandling<ExportResultEntity>(startExportRequest);
-        
-        long exportRunId = startExportResponse.Id;
-        string downloadUrl = await PollForDownloadUrl(exportRunId);
-
-        var s3Client = new RestClient();
-        var downloadS3Request = new RestRequest(downloadUrl);
-        var downloadS3Response = await s3Client.ExecuteAsync(downloadS3Request);
-
-        if (!downloadS3Response.IsSuccessful || string.IsNullOrEmpty(downloadS3Response.Content))
-            throw new PluginApplicationException($"Failed to download export file ({(int)downloadS3Response.StatusCode}).");
-
-        string exportedJson = downloadS3Response.Content;
-        var productContent = new ProductJsonContent(exportedJson);
-        var product = productContent.Products.FirstOrDefault() ?? 
-                      throw new PluginMisconfigurationException("The export contained no products");
+        var propertyKeys = product.Values.Select(x => x.Key);
+        var propertyDefinitions = await PropertyHelper.GetDefinitions(Client, propertyKeys);
         
         var doc = ProductHtmlConverter.GenerateHtml(
-            productContent, 
+            product, 
+            propertyDefinitions,
             locale, 
             includeNonLocalizable: downloadInput.OnlyLocalizableProperties is false, 
             downloadInput.ExcludeProperties);
         
+        string? nameProperty = current.GetRolePropertyId(RolePropertyNames.ProductName);
         string? productName = nameProperty is null ? null : product.GetValue(nameProperty);
         string fileName = $"{product.Id}_{locale}.html";
+        
         var coded = new HtmlCoder().Deserialize(doc.DocumentNode.OuterHtml, fileName);
-
         coded.Language = locale;
         coded.Metadata["blackbird-salsify-version"] = product.Version.ToString();
         coded.SystemReference = new SystemReference
@@ -142,35 +109,5 @@ public class ProductActions(InvocationContext context, IFileManagementClient fil
 
         var outputFile = await fileManagementClient.UploadAsync(coded.ToStream(), MediaTypeNames.Text.Html, fileName);
         return new(outputFile);
-    }
-
-    private async Task<string> PollForDownloadUrl(long exportRunId)
-    {
-        var getExportRequest = new SalsifyRequest($"export_runs/{exportRunId}", Method.Get, ApiVersion.Unversioned);
-
-        int pollingTimeoutMinutes = 15;
-        var pollingInterval = TimeSpan.FromSeconds(10);
-        int maxAttempts = (int)(TimeSpan.FromMinutes(pollingTimeoutMinutes) / pollingInterval);
-
-        string? downloadUrl = null;
-
-        for (int attempts = 0; attempts < maxAttempts; attempts++)
-        {
-            var getExportResponse = await Client.ExecuteWithErrorHandling<ExportResultEntity>(getExportRequest);
-            if (!getExportResponse.ExportEnded)
-            {
-                await Task.Delay(pollingInterval);
-                continue;
-            }
-            
-            if (getExportResponse.ExportEndedSuccessfully)
-                downloadUrl = getExportResponse.Url;
-            else
-                throw new PluginApplicationException($"Product export failed. Fail reason: {getExportResponse.FailureReason ?? "unknown"}");
-
-            break;
-        }
-        
-        return downloadUrl ?? throw new PluginApplicationException($"Export run {exportRunId} did not finish within {pollingTimeoutMinutes} minutes");
     }
 }
--- Apps.Salsify/Converters/Product/ProductHtmlConverter.cs
+++ Apps.Salsify/Converters/Product/ProductHtmlConverter.cs
@@ -1,5 +1,4 @@
 using System.Net;
-using Apps.Salsify.Converters.Product.Models;
 using Apps.Salsify.Models.Entities.Product;
 using Apps.Salsify.Models.Entities.Properties;
 using Blackbird.Applications.Sdk.Common.Exceptions;
@@ -16,35 +15,30 @@ public static class ProductHtmlConverter
     private static readonly HashSet<string> MarkupDataTypes = new(StringComparer.OrdinalIgnoreCase) { "rich_text", "html" };
 
     public static HtmlDocument GenerateHtml(
-        ProductJsonContent content,
+        ProductEntity product,
+        IReadOnlyDictionary<string, PropertyEntity> definitions,
         string locale,
         bool includeNonLocalizable = false,
         IReadOnlyCollection<string>? excludeProperties = null)
     {
-        ProductEntity product = content.Products.FirstOrDefault() ?? throw new PluginApplicationException("The export contained no products");
-
-        var definitions = content.Attributes
-            .GroupBy(x => x.Id, StringComparer.Ordinal)
-            .ToDictionary(x => x.Key, x => x.First(), StringComparer.Ordinal);
-
         var excluded = new HashSet<string>(excludeProperties ?? [], StringComparer.Ordinal);
 
         var doc = new HtmlDocument();
         doc.LoadHtml("<html><head><meta charset=\"utf-8\"></head><body></body></html>");
-        HtmlNode body = doc.DocumentNode.SelectSingleNode("//body")!;
+        var body = doc.DocumentNode.SelectSingleNode("//body")!;
 
         int emitted = 0;
-        foreach (var (propertyId, token) in product.Values)
+        foreach (var (propertyId, _) in product.Values)
         {
             var definition = definitions.GetValueOrDefault(propertyId);
-            if (excluded.Contains(propertyId) || !IsEligible(definition, includeNonLocalizable))
+            if (excluded.Contains(propertyId) || !PropertyIsEligible(definition, includeNonLocalizable))
                 continue;
 
             string lookupKey = definition!.Localizable ? locale : string.Empty;
             if (!product.GetLocalizedValues(propertyId).TryGetValue(lookupKey, out var values))
                 continue;
 
-            for (var index = 0; index < values.Count; index++)
+            for (int index = 0; index < values.Count; index++)
             {
                 body.AppendChild(BuildValueNode(doc, propertyId, index, definition.DataType, values[index]));
                 emitted++;
@@ -52,11 +46,11 @@ public static class ProductHtmlConverter
         }
 
         return emitted == 0 
-            ? throw new PluginMisconfigurationException($"Product '{product.Id}' has no translatable content for {locale}")
+            ? throw new PluginMisconfigurationException($"Product '{product.Id}' has no translatable content for {locale}") 
             : doc;
     }
 
-    private static bool IsEligible(PropertyEntity? definition, bool includeNonLocalizable)
+    private static bool PropertyIsEligible(PropertyEntity? definition, bool includeNonLocalizable)
     {
         if (definition is null) 
             return false;
