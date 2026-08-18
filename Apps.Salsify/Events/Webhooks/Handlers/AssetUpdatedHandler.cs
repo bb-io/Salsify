52d009b8762c4a824167792ce345fb33637041a9
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-08-18 13:29:11 +0300

    remove locale from AssetUpdatedHandler

--- Apps.Salsify/Events/Webhooks/Handlers/AssetUpdatedHandler.cs
+++ Apps.Salsify/Events/Webhooks/Handlers/AssetUpdatedHandler.cs
@@ -1,15 +1,11 @@
 using Apps.Salsify.Constants.Webhooks;
 using Apps.Salsify.Events.Webhooks.Handlers.Base;
-using Apps.Salsify.Models.Identifiers.Optional;
 using Blackbird.Applications.Sdk.Common.Invocation;
-using Blackbird.Applications.Sdk.Common.Webhooks;
 
 namespace Apps.Salsify.Events.Webhooks.Handlers;
 
-public class AssetUpdatedHandler(InvocationContext invocationContext, [WebhookParameter] LocaleOptionalIdentifier localeIdentifier) 
-    : BaseWebhookHandler(invocationContext)
+public class AssetUpdatedHandler(InvocationContext invocationContext) : BaseWebhookHandler(invocationContext)
 {
     protected override AlertTriggerType TriggerType => AlertTriggerType.Change;
     protected override AlertEntityType EntityType => AlertEntityType.DigitalAsset;
-    protected override IEnumerable<string?> Locales { get; } = [localeIdentifier.Locale];
 }
